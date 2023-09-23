029cbdd25741b486620fadb6254d5ba4b8987a9d
8dcc <8dcc.git@gmail.com>
2023-09-23 23:36:20 +0200

    Print Thread array instead of cJSON data
    Move TARGET_URL to util.h

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,32 +7,42 @@
 #include "dependencies/cJSON/cJSON.h"
 #include "include/util.h"
 
-#define TARGET_URL "https://a.4cdn.org/g/threads.json"
-
 int main() {
-    cJSON* pages = json_from_url(TARGET_URL);
-    if (!pages) {
-        fprintf(stderr, "4cli: Couldn't parse URL for pages\n");
-        cJSON_Delete(pages);
+    cJSON* json_threads = json_from_url(THREADS_URL);
+    if (!json_threads) {
+        PANIC("Couldn't get JSON for threads URL");
+        cJSON_Delete(json_threads);
         return 1;
     }
 
-    for (int i = 0; i < 3; i++) {
-        cJSON* page_threads = get_threads_from_page(pages, i);
-        if (!page_threads) {
-            fprintf(stderr, "4cli: Couldn't get pages[%d].threads\n", i);
-            cJSON_Delete(pages);
-            return 1;
-        }
+    Thread thread_arr[MAX_THREADS];
+    for (int i = 0; i < MAX_THREADS; i++)
+        thread_arr[i] = (Thread){
+            .id       = 0,
+            .replies  = 0,
+            .images   = 0,
+            .title    = NULL,
+            .filename = NULL,
+        };
 
-        printf("\n----[ Page %d ]----\n\n", i);
+    if (!threads_from_json(thread_arr, json_threads)) {
+        PANIC("Couldn't get Thread array from json");
+        threads_free(thread_arr);
+        cJSON_Delete(json_threads);
+        return 1;
+    }
 
-        char* tmp = cJSON_Print(page_threads);
-        printf("%s", tmp);
-        free(tmp);
+    for (int i = 0; i < MAX_THREADS; i++) {
+        if (thread_arr[i].id == 0)
+            break;
+
+        printf("%d, %d, %d, %s, %s\n", thread_arr[i].id, thread_arr[i].replies,
+               thread_arr[i].images, thread_arr[i].title,
+               thread_arr[i].filename);
     }
 
     /* Free stuff */
-    cJSON_Delete(pages);
+    threads_free(thread_arr);
+    cJSON_Delete(json_threads);
     return 0;
 }
