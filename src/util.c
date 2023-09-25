e84d336198f070c912e7264be234732160cbed5e
8dcc <8dcc.git@gmail.com>
2023-09-25 18:55:58 +0200

    Add html2txt, use our own strcpy

--- src/util.c
+++ src/util.c
@@ -22,6 +22,18 @@ typedef struct {
 
 /*----------------------------------------------------------------------------*/
 
+/* string.h function does weird stuff when strings overlap (our case) */
+static char* my_strcpy(char* dest, const char* src) {
+    size_t i;
+
+    for (i = 0; src[i] != '\0'; i++)
+        dest[i] = src[i];
+
+    dest[i] = '\0';
+
+    return dest;
+}
+
 /* Curl callback */
 static size_t parse_curl_response(char* response, size_t item_sz,
                                   size_t item_num, void* user_data) {
@@ -87,8 +99,8 @@ bool threads_from_json(Thread* out, cJSON* in) {
 
     cJSON* cur_page;
     cJSON_ArrayForEach(cur_page, in) {
-        cJSON* json_threads =
-          cJSON_GetObjectItemCaseSensitive(cur_page, "threads");
+        cJSON* json_threads = cJSON_GetObjectItemCaseSensitive(cur_page, "threa"
+                                                                         "ds");
         if (!json_threads) {
             PANIC("Couldn't get thread list cJSON object from page");
             return false;
@@ -139,11 +151,50 @@ static char* replace_html_entities(char* str) {
             *cur_ent = ents[i].c;
 
             /* Shift the rest of the string */
-            strcpy(cur_ent + 1, cur_ent + ents[i].strlen);
+            my_strcpy(cur_ent + 1, cur_ent + ents[i].strlen);
         }
     }
 
     return str;
+}
+
+static char* html2txt(char* str) {
+    char* ret = str;
+
+    bool print_newline = false;
+    bool in_label      = false;
+    char* label_start  = str;
+
+    while (*str != '\0') {
+        switch (*str) {
+            case '<':
+                in_label    = true;
+                label_start = str;
+                str++;
+                break;
+            case '>':
+                if (print_newline) {
+                    *label_start = '\n';
+                    label_start++;
+                }
+
+                in_label = false;
+                my_strcpy(label_start, str + 1);
+                str = label_start;
+                break;
+            default:
+                if (in_label) {
+                    if (*str++ == 'b' && *str++ == 'r')
+                        print_newline = true;
+                } else {
+                    str++;
+                }
+
+                break;
+        }
+    }
+
+    return ret;
 }
 
 static inline bool is_cjson_int(cJSON* p) {
